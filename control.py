41e2fa8f8d849c5fabab49212eeb3ecb7c708d95
danaument <31663507+danaument@users.noreply.github.com>
2018-06-25 13:44:35 -0500

    Update control.py

--- control.py
+++ control.py
@@ -8,6 +8,7 @@ import ds18b20
 import rpi
 import RPi.GPIO as GPIO  # pylint: disable=F0401
 import Adafruit_CharLCD as LCD
+import Adafruit_CharLCD as drivelcd
 
 # Put your device token here. To get the token,
 # sign up at https://cloud4rpi.io and create a device.
