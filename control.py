f1eadd68d7fc68f3de48ac695ccc63a19e7ddf10
danaument <31663507+danaument@users.noreply.github.com>
2018-06-25 13:42:16 -0500

    Update control.py

--- control.py
+++ control.py
@@ -7,12 +7,32 @@ import cloud4rpi
 import ds18b20
 import rpi
 import RPi.GPIO as GPIO  # pylint: disable=F0401
-import drivelcd
+import Adafruit_CharLCD as LCD
 
 # Put your device token here. To get the token,
 # sign up at https://cloud4rpi.io and create a device.
 DEVICE_TOKEN = 'BA95a661QP1RgvQu9aVw7iVKn'
 
+# Raspberry Pi pin setup
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 21
+lcd_d7 = 22
+lcd_backlight = 2
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+lcd.message('Hello\nworld!')
+# Wait 5 seconds
+
+time.sleep(5.0)
+lcd.clear()
 
 # Constants
 LED_PIN = 12
